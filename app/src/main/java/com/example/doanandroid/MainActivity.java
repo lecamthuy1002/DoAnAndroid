05568d84ef665bf3e8165148da6df791fe68a409
NgocHien-nt <tranngochie998@gmail.com>
2022-08-01 15:37:49 +0700

    errror

--- app/src/main/java/com/example/doanandroid/MainActivity.java
+++ app/src/main/java/com/example/doanandroid/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-	\\update code
+	\\update code xxx3
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
