20504b15a993bf23278fb69e114bea5bff05e9d5
NgocHien-nt <tranngochie998@gmail.com>
2022-08-02 21:42:19 +0700

    xong layout dang nhap

--- app/src/main/java/com/example/doanandroid/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/example/doanandroid/ui/gallery/GalleryFragment.java
@@ -16,17 +16,16 @@ public class GalleryFragment extends Fragment {
 
     private FragmentGalleryBinding binding;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        GalleryViewModel galleryViewModel =
-                new ViewModelProvider(this).get(GalleryViewModel.class);
-
-        binding = FragmentGalleryBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
-
-        final TextView textView = binding.textGallery;
-        galleryViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+//        GalleryViewModel galleryViewModel =
+//                new ViewModelProvider(this).get(GalleryViewModel.class);
+//
+//        binding = FragmentGalleryBinding.inflate(inflater, container, false);
+//        View root = binding.getRoot();
+//
+//        final TextView textView = binding.textGallery;
+//        galleryViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        return null;
     }
 
     @Override
