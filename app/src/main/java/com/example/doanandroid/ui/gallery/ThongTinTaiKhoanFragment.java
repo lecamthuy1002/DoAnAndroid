db7a8b90e8f0d045fc925ff51cd055017035b342
NgocHien-nt <tranngochie998@gmail.com>
2022-08-03 14:19:04 +0700

    xong layout dang nhap

---
+++ app/src/main/java/com/example/doanandroid/ui/gallery/ThongTinTaiKhoanFragment.java
@@ -0,0 +1,21 @@
+package com.example.doanandroid.ui.gallery;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+
+
+public class ThongTinTaiKhoanFragment extends Fragment {
+
+    public View onCreateView (@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.flagment, container, false);
+        return root;
+    }
+}
